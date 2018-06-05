7cbf9be667d9f8f13ae539b5c71b95ae395715fc
Alexander Ivanov <aivanov@informatica.ru>
2018-06-05 10:53:08 +0300

    first repo sync, git pull

--- server/modules/github-puller/client.js
+++ server/modules/github-puller/client.js
@@ -2,6 +2,7 @@ import { GraphQLClient } from 'graphql-request';
 import options from 'tools/options';
 import schedule from 'node-schedule';
 import logger from 'tools/logger';
+import { db } from 'tools/db/nedb';
 import path from 'path';
 import { series } from './exec';
 
@@ -39,54 +40,162 @@ export const repoWatch = repository => {
     debug('[repoWatch] ', repository.name, 'already has schedule.');
     return;
   }
-  actualize(repository).catch(err => {
-    debug('err', err.message);
-  });
-  schedules[repository.name] = schedule.scheduleJob('*/5 * * * *', function(
+  actualize({ repository, firstSync: true });
+  schedules[repository.name] = schedule.scheduleJob('*/1 * * * *', function(
     fireDate
   ) {
-    actualize(repository).catch(err => {
-      debug('err', err.message);
-    });
+    actualize({ repository });
   });
 
   debug('[repoWatch] schedule created for:', repository.name);
 };
 
-export const actualize = async repository => {
-  const { changed } = await IsRepoChanged(repository);
+const changeState = (repository, status) => {
+  db.Repository.update(
+    { _id: repository._id },
+    {
+      $set: {
+        ...{
+          status: {
+            updatedAt: new Date(),
+          },
+        },
+        ...status,
+      },
+    },
+    err => {
+      if (err) {
+        debug('changeState err', err);
+      }
+    }
+  );
+};
+
+const createOperation = operation => {
+  db.Operation.insert(operation, err => {
+    if (err) {
+      debug('createOperation err', err.message);
+    }
+  });
+};
+
+const getRepFolderPath = repository =>
+  path.resolve(process.cwd(), `../${repository.name}`);
+
+export const actualize = async ({ repository, firstSync = false }) => {
+  debug('actualize', repository.name);
+  let changed;
+  if (firstSync) {
+    let res = await repoSync(repository);
+    changed = res.changed;
+  } else {
+    let res = await IsRepoChanged(repository);
+    changed = res.changed;
+  }
   if (changed) {
     debug(`repository ${repository.name} was changed!`);
-    const repFolderPath = path.resolve(
-      process.cwd(),
-      `../${repository.name}`
-    );
-    debug('repFolderPath', repFolderPath);
-    series(['git status'], { cwd: repFolderPath }, (err, stdout, stderr) => {
-      if (err) {
-        debug(repository.name, 'err', err);
-        debug(repository.name, 'stderr', stderr);
-        throw err;
-      }
-      debug(repository.name, 'stdout', stdout);
-      const branchRe = new RegExp(`origin\/${repository.branch}`, 'g');
-      const nothingRe = new RegExp(
-        'nothing to commit, working tree clean',
-        'g'
-      );
-      if (!stdout.match(branchRe)) {
-        debug(repository.name, 'match branchRe', stdout.match(branchRe));
-        debug(`local branch is not '${repository.branch}'`);
-        // TODO change status
-      } else {
-        debug(repository.name, 'match nothingRe', stdout.match(nothingRe));
-        const isUpToDate = !!(
-          stdout.match(branchRe) && stdout.match(nothingRe)
-        );
-        debug('isUpToDate', isUpToDate);
-      }
+    changeState(repository, {
+      status: 'actualizing..',
+      message: 'remote repository has changes\'',
     });
+    const repFolderPath = getRepFolderPath(repository);
+    debug('repFolderPath', repFolderPath);
+    series(
+      ['git status'],
+      { cwd: repFolderPath },
+      async (err, stdout, stderr) => {
+        createOperation({
+          name: 'git status',
+          repoName: repository.name,
+          stdout,
+          stderr,
+          err,
+          createdAt: new Date(),
+        });
+        if (err) {
+          debug(repository.name, 'err', err);
+          debug(repository.name, 'stderr', stderr);
+          changeState(repository, {
+            status: 'exec err',
+            message: err.message,
+          });
+        } else {
+          debug(repository.name, 'stdout', stdout);
+          const branchRe = new RegExp(`origin\/${repository.branch}`, 'g');
+          const nothingRe = new RegExp(
+            'nothing to commit, working tree clean',
+            'g'
+          );
+          if (!stdout.match(branchRe)) {
+            debug(repository.name, 'match branchRe', stdout.match(branchRe));
+            debug(`local branch is not '${repository.branch}'`);
+            changeState(repository, {
+              status: 'err',
+              message: `local branch is not '${repository.branch}'`,
+            });
+          } else {
+            const readyForPull = !!(
+              stdout.match(branchRe) && stdout.match(nothingRe)
+            );
+            debug('readyForPull', readyForPull);
+
+            if (readyForPull) {
+              await gitPull(repository, repFolderPath);
+            } else {
+              changeState(repository, {
+                status: 'warning',
+                message: 'repository has uncommitted changes',
+              });
+            }
+          }
+        }
+      }
+    );
   }
+};
+
+const gitPull = (repository, repFolderPath) => {
+  return new Promise((resolve, reject) => {
+    debug('gitPull', repository.name);
+    changeState(repository, {
+      status: 'actualizing..',
+      message: 'github pulling..\'',
+    });
+    series(
+      [`git pull origin ${repository.branch}`],
+      { cwd: repFolderPath },
+      async (err, stdout, stderr) => {
+        createOperation({
+          name: 'git pull',
+          repoName: repository.name,
+          stdout,
+          stderr,
+          err,
+          createdAt: new Date(),
+        });
+        if (err) {
+          debug(repository.name, 'err', err);
+          debug(repository.name, 'stderr', stderr);
+          changeState(repository, {
+            status: 'exec err',
+            message: err.message,
+          });
+        } else {
+          debug(repository.name, 'stdout', stdout);
+          const isUpToDateRe = new RegExp('Already up-to-date');
+          if (stdout.match(isUpToDateRe)) {
+            changeState(repository, {
+              status: 'ok',
+              message: 'repository branch is up-to-date',
+            });
+          } else {
+            // TODO
+            debug('BUILD TEST CONTAINER');
+          }
+        }
+      }
+    );
+  });
 };
 
 export const connectedBy = () => {
@@ -130,27 +239,75 @@ export const fetchChanges = repository => {
   return client.request(query);
 };
 
-export const IsRepoChanged = async repository => {
-  const data = await fetchChanges(repository);
-  const lastCommit = data.repository.ref.target.history.edges[0];
-  let changed = repository.lastCommit.oid !== lastCommit.oid;
-  return { changed, lastCommit };
-};
+export const IsRepoChanged = repository =>
+  new Promise(async (resolve, reject) => {
+    const data = await fetchChanges(repository);
+    const lastCommit = data.repository.ref.target.history.edges[0].node;
+    let changed = repository.lastCommit.oid !== lastCommit.oid;
+    return resolve({ changed, lastCommit });
+  });
 
-export const repoSync = async repository => {
-  const { changed, lastCommit } = await IsRepoChanged(repository);
-  repository.lastCommit = lastCommit;
-  await db.Repository.update(
-    { _id: repository._id },
-    { $set: { lastCommit } },
-    {},
-    err => {
-      if (err) {
-        throw err;
+export const readLocalCommit = repository =>
+  new Promise((resolve, reject) => {
+    const repFolderPath = getRepFolderPath(repository);
+    series(
+      ['git log -1'],
+      { cwd: repFolderPath },
+      async (err, stdout, stderr) => {
+        createOperation({
+          name: 'git read last commit',
+          repoName: repository.name,
+          stdout,
+          stderr,
+          err,
+          createdAt: new Date(),
+        });
+        if (err) {
+          debug(repository.name, 'err', err);
+          debug(repository.name, 'stderr', stderr);
+          changeState(repository, {
+            status: 'exec err',
+            message: err.message,
+          });
+          return reject(err);
+        }
+        debug('last commit result', stdout);
+        const re = new RegExp('(commit)(.*?)(?=\\n)', 'g');
+        let commitOid = stdout.match(re);
+        commitOid = commitOid[0].replace('commit', '').trim();
+        debug('commitOid', commitOid);
+        return resolve(commitOid);
       }
-    }
-  );
-};
+    );
+  });
+
+export const repoSync = repository =>
+  new Promise(async (resolve, reject) => {
+    // TODO read last localCommit and compare with remote.
+    const localCommitOid = await readLocalCommit(repository);
+    const { changed, lastCommit } = await IsRepoChanged(repository);
+
+    repository.lastCommit = lastCommit;
+    db.Repository.update(
+      { _id: repository._id },
+      { $set: { lastCommit } },
+      {},
+      err => {
+        if (err) {
+          return reject(err);
+        }
+        if (localCommitOid === lastCommit.oid) {
+          debug(
+            `repository ${
+              repository.name
+            } synchronized. branch is up-to-date.`
+          );
+          return resolve({ changed: false });
+        }
+        return resolve({ changed });
+      }
+    );
+  });
 
 export default {
   init,
